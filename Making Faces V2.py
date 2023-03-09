9682b1688f39214538a648fce044d3b048c698e4
nodarisvanidze88 <99223872+nodarisvanidze88@users.noreply.github.com>
2023-03-09 18:18:01 +0400

    Create Making Faces V2.py

---
+++ Making Faces V2.py
@@ -0,0 +1,4 @@
+                                # convert text to emoji booring version
+text = input()                                                                  # get user's Text
+
+print(text.replace(":)" , "😀").replace(":(" , "😞").replace(":|" , "🤨",))    # run function and finaly get the result
